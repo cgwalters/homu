b77af66fcfa7a6663c93ac66b207f2a6a515f19e
Colin Walters <walters@verbum.org>
2016-03-11 16:16:31 -0500

    git: Use cwd for ssh key
    I'm trying to run homu in a container where it's installed (as root)
    to `/usr`, but we run as non-root.  This follows general best practice
    that apps shouldn't be able to mutate their code.
    
    However, this bit broke as it tried to write to `/usr`.  Let's assume
    we can write to our process' cwd instead.

--- homu/git_helper.py
+++ homu/git_helper.py
@@ -4,7 +4,7 @@ import sys
 import subprocess
 import os
 
-SSH_KEY_FILE = os.path.join(os.path.dirname(__file__), '../cache/key')
+SSH_KEY_FILE = os.path.join(os.getcwd(), 'sshkey')
 
 def main():
     args = ['ssh', '-i', SSH_KEY_FILE, '-S', 'none'] + sys.argv[1:]

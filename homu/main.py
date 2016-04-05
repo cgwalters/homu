cgwalters/homu

commit 7fa396555de01b8dd19bd78a21c64c647926e199

Fix exemption

homu/main.py
@@ -572,39 +572,39 @@ def start_build(state, repo_cfgs, buildbot_slots, logger, db, git_cfg):
     if len(builders) is 0:
         raise RuntimeError('Invalid configuration')
 
-    # if state.approved_by and builders == ['status'] and repo_cfg['status']['context'] == 'continuous-integration/travis-ci/push':
-    #     for info in utils.github_iter_statuses(state.get_repo(), state.head_sha):
-    #         if info.context == 'continuous-integration/travis-ci/pr':
-    #             if info.state == 'success':
-    #                 mat = re.search('/builds/([0-9]+)$', info.target_url)
-    #                 if mat:
-    #                     url = 'https://api.travis-ci.org/{}/{}/builds/{}'.format(state.owner, state.name, mat.group(1))
-    #                     res = requests.get(url)
-    #                     travis_sha = json.loads(res.text)['commit']
-    #                     travis_commit = state.get_repo().commit(travis_sha)
-    #                     if travis_commit:
-    #                         base_sha = state.get_repo().ref('heads/' + state.base_ref).object.sha
-    #                         if [travis_commit.parents[0]['sha'], travis_commit.parents[1]['sha']] == [base_sha, state.head_sha]:
-    #                             try:
-    #                                 merge_sha = create_merge(state, repo_cfg, state.base_ref, git_cfg)
-    #                             except subprocess.CalledProcessError:
-    #                                 print('* Unable to create a merge commit for the exempted PR: {}'.format(state))
-    #                                 traceback.print_exc()
-    #                             else:
-    #                                 if merge_sha:
-    #                                     desc = 'Test exempted'
-    #                                     url = info.target_url
+    if state.approved_by and len(builders) == 1 and len(repo_cfg['status']) == 1 and 'context' in repo_cfg['status'][0] and repo_cfg['status'][0]['context'] == 'continuous-integration/travis-ci/push':
+        for info in utils.github_iter_statuses(state.get_repo(), state.head_sha):
+            if info.context == 'continuous-integration/travis-ci/pr':
+                if info.state == 'success':
+                    mat = re.search('/builds/([0-9]+)$', info.target_url)
+                    if mat:
+                        url = 'https://api.travis-ci.org/{}/{}/builds/{}'.format(state.owner, state.name, mat.group(1))
+                        res = requests.get(url)
+                        travis_sha = json.loads(res.text)['commit']
+                        travis_commit = state.get_repo().commit(travis_sha)
+                        if travis_commit:
+                            base_sha = state.get_repo().ref('heads/' + state.base_ref).object.sha
+                            if [travis_commit.parents[0]['sha'], travis_commit.parents[1]['sha']] == [base_sha, state.head_sha]:
+                                try:
+                                    merge_sha = create_merge(state, repo_cfg, state.base_ref, git_cfg)
+                                except subprocess.CalledProcessError:
+                                    print('* Unable to create a merge commit for the exempted PR: {}'.format(state))
+                                    traceback.print_exc()
+                                else:
+                                    if merge_sha:
+                                        desc = 'Test exempted'
+                                        url = info.target_url
 
-    #                                     state.set_status('success')
-    #                                     utils.github_create_status(state.get_repo(), state.head_sha, 'success', url, desc, context='homu')
-    #                                     state.add_comment(':zap: {} - [{}]({})'.format(desc, 'status', url))
+                                        state.set_status('success')
+                                        utils.github_create_status(state.get_repo(), state.head_sha, 'success', url, desc, context='homu')
+                                        state.add_comment(':zap: {} - [{}]({})'.format(desc, 'status', url))
 
-    #                                     state.merge_sha = merge_sha
-    #                                     state.save()
+                                        state.merge_sha = merge_sha
+                                        state.save()
 
-    #                                     state.fake_merge(repo_cfg)
-    #                                     return True
-    #             break
+                                        state.fake_merge(repo_cfg)
+                                        return True
+                break
 
     merge_sha = create_merge(state, repo_cfg, branch, git_cfg)
     if not merge_sha:

commit 20fa05e6b9e52a67b093a19bdc0f7f3bf915a7d6

Fix travis exemption code again for status-only context
It seems that
https://github.com/servo/homu/pull/13/commits/7fa396555de01b8dd19bd78a21c64c647926e199
regressed things when using status only.

The format seems to have changed at one point to a dictionary, but
this code was treating it as a list (using `[0]`).

Since we were already iterating over the statuses, move the special
casing for "status-only-travis" up to there, and slay the massive
conditional.

Conflicts:
	homu/main.py

homu/main.py
@@ -571,16 +571,29 @@ def start_build(state, repo_cfgs, buildbot_slots, logger, db, git_cfg):
     builders = []
     branch = 'try' if state.try_ else 'auto'
     branch = repo_cfg.get('branch', {}).get(branch, branch)
+    do_travis_exemption = False
     if 'buildbot' in repo_cfg:
         builders += repo_cfg['buildbot']['try_builders' if state.try_ else 'builders']
     if 'travis' in repo_cfg:
         builders += ['travis']
     if 'status' in repo_cfg:
-        builders += ['status-' + key for key, value in repo_cfg['status'].items() if 'context' in value]
+        found_travis_context = False
+        for key, value in repo_cfg['status'].items():
+            context = value.get('context')
+            if context is not None:
+                builders += ['status-' + key]
+                # We have an optional fast path if the Travis test passed for a given
+                # commit and master is unchanged, we can do a direct push.
+                if context == 'continuous-integration/travis-ci/push':
+                    found_travis_context = True
+                
+        if found_travis_context and len(builders) == 1:
+            do_travis_exemption = True
+
     if len(builders) is 0:
         raise RuntimeError('Invalid configuration')
 
-    if state.approved_by and len(builders) == 1 and 'status' in repo_cfg and len(repo_cfg['status']) == 1 and 'context' in repo_cfg['status'][0] and repo_cfg['status'][0]['context'] == 'continuous-integration/travis-ci/push':
+    if (state.approved_by and do_travis_exemption):
         for info in utils.github_iter_statuses(state.get_repo(), state.head_sha):
             if info.context == 'continuous-integration/travis-ci/pr':
                 if info.state == 'success':